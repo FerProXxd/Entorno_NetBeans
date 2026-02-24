e6509c8b53635bd70305589e97aa15be3a5b158f
Fernando <Fernando@192.168.32.66>
2026-02-24 17:04:56 +0100

    Este es el primer commit.

---
+++ src/proyecto_entorno/Proyecto_Entorno.java
@@ -0,0 +1,12 @@
+package proyecto_entorno;
+
+public class Proyecto_Entorno {
+
+    public static void main(String[] args) {
+
+        System.out.println("Esto es el proyecto recién creado");
+
+
+    }
+    
+}
