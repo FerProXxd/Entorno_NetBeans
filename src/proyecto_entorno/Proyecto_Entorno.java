b72b2555e1d3908c6e07cf5424ff28d58cff094d
Fernando <Fernando@192.168.32.66>
2026-02-24 17:18:49 +0100

    Este es el segundo commit

--- src/proyecto_entorno/Proyecto_Entorno.java
+++ src/proyecto_entorno/Proyecto_Entorno.java
@@ -5,6 +5,10 @@ public class Proyecto_Entorno {
     public static void main(String[] args) {
 
         System.out.println("Esto es el proyecto recién creado");
+        
+        System.out.println("Esto es la primera línea de la rama2");
+        
+        System.out.println("Esto es la segunda línea de la rama2");
 
 
     }
